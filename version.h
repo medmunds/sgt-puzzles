3daff5a3e7745a4ca9f06697d6abc6c66e05a251
Mike Edmunds <medmunds@gmail.com>
2025-05-09 15:49:26 -0700

    puzzles-web: allow defining version in C_FLAGS

--- version.h
+++ version.h
@@ -6,8 +6,15 @@
  * completely overwrites this file with information appropriate to
  * that build. The information _here_ is default stuff used for local
  * development runs of 'make'.
+ *
+ * Build environments can also supply version information via C_FLAGS:
+ * -DVER="Version YYYYmmdd.VCSID" -DVERSIONINFO_BINARY_VERSION=1,YYYY,mm,dd
  */
 
+#ifndef VER
 #define VER "Unidentified build"
+#endif
 
+#ifndef VERSIONINFO_BINARY_VERSION
 #define VERSIONINFO_BINARY_VERSION 0,0,0,0
+#endif
